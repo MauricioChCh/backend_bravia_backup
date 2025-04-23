5cbc35bd24dff2d2175f2733179aef72e5381c0d
Mau <mauricio.chaves.chaves@est.una.ac.cr>
2025-04-23 01:11:55 -0600

    Inicializacion del proyecto con springboot y configuracion de base de datos postgres

---
+++ src/main/kotlin/org/example/backendoportuniabravo/BackendOportuniaBravoApplication.kt
@@ -0,0 +1,11 @@
+package org.example.backendoportuniabravo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendOportuniaBravoApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendOportuniaBravoApplication>(*args)
+}
