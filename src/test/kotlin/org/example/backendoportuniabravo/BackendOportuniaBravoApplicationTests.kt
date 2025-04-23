704b3acd8759a7429de2dbaaba7e3a1a266daa02
Mau <mauricio.chaves.chaves@est.una.ac.cr>
2025-04-23 01:11:55 -0600

    Inicializacion del proyecto con springboot y configuracion de base de datos postgres

---
+++ src/test/kotlin/org/example/backendoportuniabravo/BackendOportuniaBravoApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.backendoportuniabravo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendOportuniaBravoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
