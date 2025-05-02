4e70ad119b2f150d18756bba5a94b51177e9bd0d
Joshua <joshyarit07@gmail.com>
2025-05-02 08:58:21 -0600

    Add create, update, and delete operations for companies
    Implemented user and profile creation logic in CompanyService during company creation. Enhanced CompanyController with endpoints for creating, updating, and deleting companies, including proper HTTP responses and validation handling.

--- src/main/kotlin/org/example/backendoportuniabravo/controller/CompanyController.kt
+++ src/main/kotlin/org/example/backendoportuniabravo/controller/CompanyController.kt
@@ -1,6 +1,11 @@
 package org.example.backendoportuniabravo.controller
 
+import org.example.backendoportuniabravo.dto.CompanyUserInput
+import org.example.backendoportuniabravo.dto.CompanyUserResult
+import org.example.backendoportuniabravo.dto.CompanyUserUpdate
 import org.example.backendoportuniabravo.service.CompanyService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -12,7 +17,18 @@ class CompanyController (private val service: CompanyService) {
     @ResponseBody
     fun getCompanyById(@PathVariable id : Long) = service.findById(id)
 
+    @PostMapping
+    fun createCompany(@RequestBody company: CompanyUserInput) : ResponseEntity<CompanyUserResult>? {
+        val saved = service.create(company)
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved)
+    }
 
+    @PutMapping
+    @ResponseBody
+    fun updateCompany(@RequestBody company: CompanyUserUpdate) = service.update(company)
 
+    @DeleteMapping("{id}")
+    @ResponseBody
+    fun deleteCompany(@PathVariable id: Long) = service.deleteById(id)
 
 }
--- src/main/kotlin/org/example/backendoportuniabravo/service/CompanyService.kt
+++ src/main/kotlin/org/example/backendoportuniabravo/service/CompanyService.kt
@@ -5,8 +5,13 @@ import org.example.backendoportuniabravo.dto.CompanyUserResponse
 import org.example.backendoportuniabravo.dto.CompanyUserResult
 import org.example.backendoportuniabravo.dto.CompanyUserUpdate
 import org.example.backendoportuniabravo.entity.Company
+import org.example.backendoportuniabravo.entity.Profile
+import org.example.backendoportuniabravo.entity.User
 import org.example.backendoportuniabravo.mapper.CompanyMapper
+import org.example.backendoportuniabravo.repository.BusinessAreaRepository
 import org.example.backendoportuniabravo.repository.CompanyRepository
+import org.example.backendoportuniabravo.repository.ProfileRepository
+import org.example.backendoportuniabravo.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.util.Optional
@@ -24,14 +29,46 @@ class CompanyServiceImpl(
     @Autowired
     private val companyRepository: CompanyRepository,
     @Autowired
-    private val companyMapper: CompanyMapper
+    private val companyMapper: CompanyMapper,
+    @Autowired
+    private val userRepository: UserRepository,
+    @Autowired
+    private val profileRepository: ProfileRepository,
+    @Autowired
+    private val bussinessAreaRepository: BusinessAreaRepository
 ) : CompanyService {
 
-    override fun create(companyUserInput: CompanyUserInput): CompanyUserResult? {
-        val companyUser = companyMapper.companyUserInputToCompany(companyUserInput)
-        return companyMapper.companyToCompanyUserResult(
-            companyRepository.save(companyUser)
+    override fun create(input: CompanyUserInput): CompanyUserResult? {
+        val userInput = input.user ?: throw IllegalArgumentException("User input cannot be null")
+
+        val user = userRepository.save(
+            User(
+                firstName = userInput.name ?: throw IllegalArgumentException("User name cannot be null"),
+                lastName = userInput.lastName ?: throw IllegalArgumentException("User last name cannot be null"),
+                email = userInput.email ?: throw IllegalArgumentException("User email cannot be null"),
+                password = userInput.password ?: throw IllegalArgumentException("User password cannot be null"),
+                createDate = java.util.Date(),
+                tokenExpired = false,
+                enabled = true
+            )
         )
+
+        val profile = profileRepository.save(Profile(user = user, verified = false))
+
+        val businessArea = input.businessArea ?.let {
+            bussinessAreaRepository.findById(it.id ?: throw IllegalArgumentException("Business area id cannot be null"))
+                .orElseThrow {throw IllegalArgumentException("Business area not found with ID: ${it.id}}")}
+        } ?: throw IllegalArgumentException("Business area must be provided")
+
+        val company = Company(
+            profile = profile,
+            name = input.name ?: throw IllegalArgumentException("Company name is required"),
+            businessAreas = mutableSetOf(businessArea),
+            description = "",
+            location = null
+        )
+
+        return companyMapper.companyToCompanyUserResult(companyRepository.save(company))
     }
 
     @Throws(NoSuchElementException::class)
