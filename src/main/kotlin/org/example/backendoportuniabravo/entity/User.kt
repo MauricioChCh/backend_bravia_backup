f667c448a23699e45b55eef136eb2c0f2966d3ba
Joshua <joshyarit07@gmail.com>
2025-05-19 12:33:12 -0600

    Add Role and Privilege entities with related mappings
    Introduced Role and Privilege entities to support role-based access control. Updated the User entity to establish relationships with these new entities. Added RoleRepository to manage role-specific database operations.

--- src/main/kotlin/org/example/backendoportuniabravo/entity/User.kt
+++ src/main/kotlin/org/example/backendoportuniabravo/entity/User.kt
@@ -1,57 +1,134 @@
 package org.example.backendoportuniabravo.entity
 
-    import jakarta.persistence.*
-    import java.util.*
+import jakarta.persistence.*
+import java.util.*
 
-    @Entity
-    @Table(name = "app_user") // "user" is reserved in PostgresSQL
-    data class User(
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Column(name = "id")
-        var id: Long? = null,
+@Entity
+@Table(name = "app_user") // "user" is reserved in PostgresSQL
+data class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    var id: Long? = null,
 
-        @Column(name = "create_date", nullable = true)
-        @Temporal(TemporalType.TIMESTAMP)
-        var createDate: Date? = Date(),
+    @Column(name = "create_date", nullable = true)
+    @Temporal(TemporalType.TIMESTAMP)
+    var createDate: Date? = Date(),
 
-        @Column(name = "first_name", nullable = false)
-        var firstName: String,
+    @Column(name = "first_name", nullable = false)
+    var firstName: String,
 
-        @Column(name = "last_name", nullable = false)
-        var lastName: String,
+    @Column(name = "last_name", nullable = false)
+    var lastName: String,
 
-        @Column(name = "email", nullable = false)
-        var email: String,
+    @Column(name = "email", nullable = false)
+    var email: String,
 
-        @Column(name = "password", nullable = false)
-        var password: String,
+    @Column(name = "password", nullable = false)
+    var password: String,
 
-        @Column(name = "token_expired", nullable = true)
-        var tokenExpired: Boolean? = false,
+    @Column(name = "token_expired", nullable = true)
+    var tokenExpired: Boolean? = false,
 
-        @Column(name = "enabled")
-        var enabled: Boolean? = true,
+    @Column(name = "enabled")
+    var enabled: Boolean = true,
 
-        @OneToOne(cascade = [CascadeType.ALL], orphanRemoval = true, fetch = FetchType.LAZY)
-        @JoinColumn(name = "profile_id")
-        var profile: Profile? = null,
+    @OneToOne(cascade = [CascadeType.ALL], orphanRemoval = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_id")
+    var profile: Profile? = null,
 
-        @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
-        var reportsMade: MutableSet<UserReports>? = mutableSetOf(),
+    @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
+    var reportsMade: MutableSet<UserReports>? = mutableSetOf(),
 
-        @OneToMany(mappedBy = "userReported", cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
-        var reportsReceived: MutableSet<UserReports>? = mutableSetOf(),
+    @OneToMany(mappedBy = "userReported", cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
+    var reportsReceived: MutableSet<UserReports>? = mutableSetOf(),
 
-        ) {
-        override fun hashCode(): Int {
-            return id?.hashCode() ?: 0
-        }
+    @ManyToMany
+    @JoinTable(
+        name = "user_role",
+        joinColumns = [JoinColumn(name = "user_id", referencedColumnName = "id")],
+        inverseJoinColumns = [JoinColumn(name = "role_id", referencedColumnName = "id")]
+    )
+    var roleList: MutableSet<Role> = mutableSetOf(),
 
-        override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            if (other !is User) return false
-            if (id == null || other.id == null) return false
-            return id == other.id
-        }
+    ) {
+    override fun hashCode(): Int {
+        return id?.hashCode() ?: 0
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is User) return false
+        if (id == null || other.id == null) return false
+        return id == other.id
+    }
+}
+
+
+@Entity
+@Table(name = "role")
+data class Role(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null,
+
+    @Column(name = "name", nullable = false, unique = true)
+    var name: String,
+
+    @ManyToMany
+    @JoinTable(
+        name = "role_privilege",
+        joinColumns = [JoinColumn(name = "role_id", referencedColumnName = "id")],
+        inverseJoinColumns = [JoinColumn(name = "privilege_id", referencedColumnName = "id")]
+    )
+    var privilegeList: MutableSet<Privilege> = mutableSetOf(),
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Role) return false
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id?.hashCode() ?: 0
+    }
+
+    override fun toString(): String {
+        return "Role(id=$id, name='$name', privilegeList=$privilegeList)"
+    }
+
+}
+
+@Entity
+@Table(name = "privilege")
+data class Privilege(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long? = null,
+    var name: String,
+    // Entity Relationship
+    @ManyToMany(fetch = FetchType.LAZY)
+    var userList: MutableSet<User> = mutableSetOf(),
+    @ManyToMany(fetch = FetchType.LAZY)
+    var roleList: MutableSet<Role> = mutableSetOf(),
+
+    ) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Privilege) return false
+
+        if (id != other.id) return false
+
+        return true
+    }
+    override fun hashCode(): Int {
+        return id?.hashCode() ?: 0
+    }
+
+    override fun toString(): String {
+        return "Privilege(id=$id, name='$name', userList=$userList, roleList=$roleList)"
+    }
+}
--- src/main/kotlin/org/example/backendoportuniabravo/repository/UserRepositories.kt
+++ src/main/kotlin/org/example/backendoportuniabravo/repository/UserRepositories.kt
@@ -1,9 +1,12 @@
 package org.example.backendoportuniabravo.repository
 
 import org.example.backendoportuniabravo.entity.Profile
+import org.example.backendoportuniabravo.entity.Role
 import org.example.backendoportuniabravo.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface UserRepository : JpaRepository<User, Long> {
@@ -16,3 +19,8 @@ interface UserRepository : JpaRepository<User, Long> {
 
 @Repository
 interface ProfileRepository : JpaRepository<Profile, Long>
+
+@Repository
+interface RoleRepository : JpaRepository<org.example.backendoportuniabravo.entity.Role, Long> {
+    fun findByName(@Param("name") name: String): Optional<Role>
+}
